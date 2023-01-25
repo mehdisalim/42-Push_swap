f5f45cde1ce26b41dd8db12d349212e3221214b4
mehdisalim <mehdisalimofficial2000@gmail.com>
2023-01-25 18:40:12 +0100

    fix error in main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: esalim <esalim@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 13:27:44 by esalim            #+#    #+#             */
-/*   Updated: 2023/01/25 12:36:14 by esalim           ###   ########.fr       */
+/*   Updated: 2023/01/25 17:49:38 by esalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,19 @@ int check_isdigits(char *str)
     return (1);
 }
 
+int get_num_count(char   **num)
+{
+    int i = -1;
+    while (num[++i]);
+    return (i);
+}
+
 void  add_to_list(t_list **head, char   **num)
 {
     int i = 0;
-    int number;
-
+    int *number = malloc(get_num_count(num) * 4);
+    if (!number)
+        return ;
     while (num[i])
     {
         if (!check_isdigits(num[i]))
@@ -34,8 +42,8 @@ void  add_to_list(t_list **head, char   **num)
             ft_putstr_fd("Error: the argement should be a digits\n", 2);
             exit(1);
         }
-        number = ft_atoi(num[i]);
-        ft_lstadd_back(head, ft_lstnew(&number));
+        number[i] = ft_atoi(num[i]);
+        ft_lstadd_back(head, ft_lstnew(&number[i]));
         i++;
     }
 }
@@ -66,16 +74,14 @@ char    *get_all_args(int ac, char **av)
     return (res);
 }
 
+
+
 int main(int ac, char **av)
 {
     char    *args = get_all_args(ac, av);
     char    **numbers = ft_split(args, ' ');
     t_list  *head = NULL;
     add_to_list(&head, numbers);
-    while (head)
-    {
-        ft_printf("%d\n", *(int *)(head->content));
-        head = head->next;
-    }
+
     return (0);
 }
